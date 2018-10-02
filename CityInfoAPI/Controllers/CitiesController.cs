df6786a6aedfb88986bd68a48ede55e220500e2e
bherring_paucar <bpaucar@inventarte.net>
2018-10-02 17:48:43 -0500

    first commit

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Controllers
+{
+
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        /*[HttpGet()]
+        public JsonResult GetCities()
+        {*/
+            /*return new JsonResult(new List<Object>()
+            {
+                new { id=1, Name="New York City"},
+                new { id=2, Name="Antwerp"},
+            });*/
+
+            //return new JsonResult(CitiesDataStore.Current.Cities);
+        //}
+
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            /*var temp = new JsonResult(CitiesDataStore.Current.Cities);
+            temp.StatusCode = 200;
+            return temp;*/
+
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        
+        /*[HttpGet("{id}")] 
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }*/
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+        }
+    }
+}
---
+++ CityInfoAPI/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace CityInfoAPI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+
+                //PERMITE HACER QUE LA PRIMERA LETRA DE CADA PROPIEDAD EN EL JSON ESTE EN MAYUSCULA
+                /*.AddJsonOptions(o => {
+                    if (o.SerializerSettings.ContractResolver != null)
+                    {
+                        var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+
+                        castedResolver.NamingStrategy = null;
+                    }
+                });*/
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
+            }
+
+
+            app.UseStatusCodePages();
+            app.UseMvc();
+
+            /*app.Run((context) => {
+
+                throw new Exception("Examepl exception");
+            });*/
+
+            /*
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });*/
+        }
+    }
+}
